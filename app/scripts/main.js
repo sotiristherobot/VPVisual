84d86c3d5bf22feb1f0455b6d4d178ca3d067e08
Sotiris Ioannou <me@sotiris.engineer>
2016-08-21 19:22:18 -0400

    Added scroll to bottom event listener to down arrow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,30 @@ for (var i = 0; i < 1000; i++){
 }
 
 console.log(sum);
+$(document).ready(function(){
+  $(window).bind('scroll', function() {
+    var navHeight = $( window ).height() - 70;
+    if ($(window).scrollTop() > navHeight) {
+      $('nav').addClass('fixing');
+      $('#main-image').addClass('animated fadeIn');
+
+
+    }
+    else {
+
+      $('nav').removeClass('fixing');
+      $('#main-image').removeClass('animated fadeIn');
+
+    }
+  });
+
+
+  //scroll to bottom
+  $('.downarrow').click(function () {
+
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    return false;
+  });
+
+});
+
